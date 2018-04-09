8374fdf6c35f46562ed22ee2ed729c26fe945de1
RF <34131940+drrf@users.noreply.github.com>
2018-04-09 09:19:15 +0300

    mmn12 -> new file

---
+++ mmn12/my_bcmp/main.c
@@ -0,0 +1,46 @@
+/*
+	
+	demo.c -- Demo Explain()
+	
+	Copyright © 2018 Ron .F.
+	
+	
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+ *          File: main.c
+ *        Author: Ron F. <>
+ * Last Modified: April 9, 2018
+ *         Topic: ...
+ * ----------------------------------------------------------------
+ */
+ 
+#include <stdio.h>
+
+int main ()
+{
+	int x;
+	/* DEMO COMMENT */
+	printf("\nDEMO TEMPLATE\n");
+	printf("\n--------------------\n");
+	
+	printf("\n enter 1 numbers\n");
+	scanf("%d",&x);
+	
+	printf("\n--------------------\n");
+	printf("\n the number is: %d \n", x);
+	
+	return 0;
+}
+
